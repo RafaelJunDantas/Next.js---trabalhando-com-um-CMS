3b02f8f945c4b1cb7b57111e639f34c9f4a5e8cb
Rafael <rafajdantas@gmail.com>
2024-06-06 10:01:35 -0300

    implement global footer from cms

--- src/infra/cms/cmsService.js
+++ src/infra/cms/cmsService.js
@@ -1,5 +1,13 @@
 const TOKEN = process.env.DATO_TOKEN_KEY_READ;
 
+const globalQuery = `
+  query {
+    globalFooter{
+      description
+    }
+  }
+`;
+
 export async function cmsService({ query }) {
 
   try {
@@ -17,14 +25,33 @@ export async function cmsService({ query }) {
       const body = await res.json();
       //console.log('Body: ', body);
       if(!body.errors) return body;
-
       throw new Error(JSON.stringify(body));
     });
 
-    //console.log('Page content res: ', pageContentRes);
+    const globalContentRes = await fetch('https://graphql.datocms.com/', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+        'Authorization': 'Bearer ' + TOKEN,
+      },
+      body: JSON.stringify({
+        query: globalQuery,
+      }),
+    })
+    .then(async (res) => {
+      const body = await res.json();
+      //console.log('Body: ', body);
+      if(!body.errors) return body;
+      throw new Error(JSON.stringify(body));
+    });
 
     return {
-      data: pageContentRes.data,
+      data: {
+        ...pageContentRes.data,
+        globalContent: {
+          ...globalContentRes.data,
+        } 
+      },
     }
   }catch(err) {
     throw new Error(err.message);
--- src/screens/FAQQuestionScreen/index.js
+++ src/screens/FAQQuestionScreen/index.js
@@ -37,14 +37,13 @@ export async function getStaticProps({ params }) {
 
   return {
     props: {
+      cmsContent: data,
       id,
-      title: data.contentFaqQuestion.title,
-      content: data.contentFaqQuestion.content,
     }
   }
 }
 
-export default function FAQQuestionScreen({ title, content }) {
+export default function FAQQuestionScreen({ cmsContent }) {
   return (
     <>
       <Head>
@@ -73,15 +72,10 @@ export default function FAQQuestionScreen({ title, content }) {
           }}
         >
           <Text tag="h1" variant="heading1">
-            {title}
+            {cmsContent.contentFaqQuestion.title}
           </Text>
-
-          {/* <Box dangerouslySetInnerHTML={{ __html: content }} /> */}
-          {/* <pre>
-            {JSON.stringify(content, null, 4)}
-          </pre> */}
           <StructuredText 
-            data={content}
+            data={cmsContent.contentFaqQuestion.content}
             customNodeRules={[
               renderNodeRule(isHeading, ({ node, children, key }) => {
                 const tag = `h${node.level}`;
@@ -96,8 +90,8 @@ export default function FAQQuestionScreen({ title, content }) {
           />
         </Box>
       </Box>
-
-      <Footer />
+      
+      <Footer description={cmsContent.globalContent.globalFooter.description}/>
     </>
   )
 }
