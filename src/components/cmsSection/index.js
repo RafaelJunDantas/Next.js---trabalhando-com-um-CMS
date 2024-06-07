52b486699d406d2565968c9dcfc373919f1ff9af
Rafael <rafajdantas@gmail.com>
2024-06-07 10:26:38 -0300

    create and implement PageFaqQuestionsSection

--- src/components/cmsSection/index.js
+++ src/components/cmsSection/index.js
@@ -2,8 +2,10 @@ import { SEOBlock } from "./SEOBlock";
 import { Footer } from "../commons/Footer";
 import { Menu } from "../commons/Menu";
 import { PagehomeHeroSection } from "./PagehomeHeroSection";
+import { PageFAQQuestionsSection } from "./PageFAQQuestionsSection";
 
 export const cmsSections = {
+    PagefaqQuestionsSectionRecord: (props) => <PageFAQQuestionsSection {...props}/>,
     CommonSeoBlockRecord: (props) => <SEOBlock {...props}/>,
     CommonMenuRecord: (props) => <Menu {...props}/>,
     PagehomeHeroSectionRecord: (props) => <PagehomeHeroSection {...props}/>,
